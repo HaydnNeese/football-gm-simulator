bc60eff0c80f3f4765b94d64997964e776fd1591
Haydn Neese <haydnneese@yahoo.com>
2025-10-06 14:04:15 -0500

    typography

--- client/src/screens/TeamSelect/TeamDashboard/TeamDashboard.tsx
+++ client/src/screens/TeamSelect/TeamDashboard/TeamDashboard.tsx
@@ -28,12 +28,9 @@ function TeamDashboard({ team }: { team: Team | null }) {
     setTabValue(newValue);
   };
   return (
-    <Box
-      className="flex flex-row h-full w-full py-8 pr-8"
-      sx={{ fontFamily: "Anton, sans-serif" }}
-    >
+    <Box className="flex flex-row h-[90vh] w-full py-8 pr-8">
       <Box className="basis-1/4">
-      {/* TODO need transparent player images for the rest of the teams */}
+        {/* TODO need transparent player images for the rest of the teams */}
         <img
           src={PlayerTransparentPhotoMap[team?.abbreviation!]}
           alt="Star Player Image"
@@ -44,12 +41,15 @@ function TeamDashboard({ team }: { team: Team | null }) {
       <Card
         className="flex flex-col flex-1 h-full rounded-2xl!"
         sx={{
-          border: `3px solid ${team?.colorPrimary}`,
+          border: `5px solid ${team?.colorPrimary}`,
+          overflow: "hidden",
+          backgroundColor: "transparent",
         }}
       >
         <Box
+          role="card-header"
           className="flex flex-row gap-4 items-center justify-between pr-4 text-white"
-          sx={{ backgroundColor: team?.colorPrimary }}
+          sx={{ backgroundColor: team?.colorPrimary, flexShrink: 0 }}
         >
           <Box className="p-4">
             <Typography variant="h5" className="caret-transparent">
@@ -64,62 +64,89 @@ function TeamDashboard({ team }: { team: Team | null }) {
             Purchase Team
           </Button>
         </Box>
-        {/* TODO the left and right arrow key press works on these tabs as well
+        <Box className="flex-1 bg-white">
+          {/* TODO the left and right arrow key press works on these tabs as well
            as my custom team selector, need it to only work on team selector */}
-        <Tabs
-          value={tabValue}
-          onChange={handleChange}
-          variant="fullWidth"
-          aria-label="team select tabs"
-          sx={{
-            "& .MuiTab-root": {
-              color: team?.colorTertiary ?? "primary",
-            },
-            "& .Mui-selected": {
-              color: team?.colorPrimary ?? "primary",
-            },
-            "& .MuiTabs-indicator": {
-              backgroundColor: team?.colorPrimary ?? "primary",
-            },
-          }}
-        >
-          <Tab value="overview" label="Overview" />
-          <Tab value="roster" label="Roster" />
-          <Tab value="staff" label="Staff" />
-          <Tab value="stadium" label="Stadium" />
-          <Tab value="finance" label="Finance" />
-          <Tab value="draft" label="Draft Picks" />
-        </Tabs>
+          <Tabs
+            value={tabValue}
+            onChange={handleChange}
+            variant="fullWidth"
+            aria-label="team select tabs"
+            sx={{
+              "& .MuiTab-root": {
+                color: team?.colorTertiary ?? "primary",
+              },
+              "& .Mui-selected": {
+                color: team?.colorPrimary ?? "primary",
+              },
+              "& .MuiTabs-indicator": {
+                backgroundColor: team?.colorPrimary ?? "primary",
+              },
+            }}
+          >
+            <Tab value="overview" label="Overview" />
+            <Tab value="roster" label="Roster" />
+            <Tab value="staff" label="Staff" />
+            <Tab value="stadium" label="Stadium" />
+            <Tab value="finance" label="Finance" />
+            <Tab value="draft" label="Draft Picks" />
+          </Tabs>
 
-        <TabPanel value={tabValue} index="overview">
-          {/* <p>Quick summary: team identity, record, star players, fan base.</p> */}
-          <TeamOverview team={team} />
-        </TabPanel>
+          <TabPanel
+            value={tabValue}
+            index="overview"
+            backgroundColor={team?.colorPrimary!}
+            
+          >
+            {/* <p>Quick summary: team identity, record, star players, fan base.</p> */}
+            <TeamOverview team={team} />
+          </TabPanel>
 
-        <TabPanel value={tabValue} index="roster">
-          <Typography variant="h6">Roster</Typography>
-          <p>Player list, depth chart, positions.</p>
-        </TabPanel>
+          <TabPanel
+            value={tabValue}
+            index="roster"
+            backgroundColor={team?.colorPrimary!}
+          >
+            <Typography variant="h6">Roster</Typography>
+            <p>Player list, depth chart, positions.</p>
+          </TabPanel>
 
-        <TabPanel value={tabValue} index="staff">
-          <Typography variant="h6">Staff</Typography>
-          <p>Coaches, GM, coordinators.</p>
-        </TabPanel>
+          <TabPanel
+            value={tabValue}
+            index="staff"
+            backgroundColor={team?.colorPrimary!}
+          >
+            <Typography variant="h6">Staff</Typography>
+            <p>Coaches, GM, coordinators.</p>
+          </TabPanel>
 
-        <TabPanel value={tabValue} index="stadium">
-          <Typography variant="h6">Stadium</Typography>
-          <p>Stadium name, capacity, facilities.</p>
-        </TabPanel>
+          <TabPanel
+            value={tabValue}
+            index="stadium"
+            backgroundColor={team?.colorPrimary!}
+          >
+            <Typography variant="h6">Stadium</Typography>
+            <p>Stadium name, capacity, facilities.</p>
+          </TabPanel>
 
-        <TabPanel value={tabValue} index="finance">
-          <Typography variant="h6">Finance</Typography>
-          <p>Salary cap, contracts, revenue streams.</p>
-        </TabPanel>
+          <TabPanel
+            value={tabValue}
+            index="finance"
+            backgroundColor={team?.colorPrimary!}
+          >
+            <Typography variant="h6">Finance</Typography>
+            <p>Salary cap, contracts, revenue streams.</p>
+          </TabPanel>
 
-        <TabPanel value={tabValue} index="draft">
-          <Typography variant="h6">Draft Picks</Typography>
-          <p>Upcoming draft picks and trade history.</p>
-        </TabPanel>
+          <TabPanel
+            value={tabValue}
+            index="draft"
+            backgroundColor={team?.colorPrimary!}
+          >
+            <Typography variant="h6">Draft Picks</Typography>
+            <p>Upcoming draft picks and trade history.</p>
+          </TabPanel>
+        </Box>
       </Card>
     </Box>
   );
