69e3a70a495bbb5dbbd7f2776ae6fe857764bf3c
Haydn Neese <haydnneese@yahoo.com>
2025-10-06 14:02:16 -0500

    typography

--- client/src/components/layout/LabelValue.tsx
+++ client/src/components/layout/LabelValue.tsx
@@ -1,12 +1,11 @@
 import Box from "@mui/material/Box";
-import React from "react";
 import Typography from '@mui/material/Typography';
 
 function LabelValue({label, value}: {label: string, value: string | number | Date}) {
     return (
-        <Box className="flex flex-row gap-2 items-baseline">
+        <Box className="flex flex-row gap-2 items-center justify-between">
             <Typography variant="subtitle1">{label}:</Typography>
-            <Typography variant="body2">{value instanceof Date ? value.toLocaleDateString() : value}</Typography>
+            <Typography className="text-xs!">{value instanceof Date ? value.toLocaleDateString() : value}</Typography>
         </Box>
     );
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,42 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 
 const theme = createTheme({
   palette: {
     mode: "light", // or "dark"
-    primary: { main: "#1976d2" },
-    secondary: { main: "#dc004e" },
+    info: { main: "#1976d2" },
+    error: { main: "#dc0000"},
+    warning: { main: "#dca900"},
+    success: { main: "#41e01d"}
   },
   typography: {
-    fontFamily: "Anton, sans-serif",
-  },
-  components: {
-    MuiButton: {
-      styleOverrides: {
-        root: {
-          fontFamily: "Anton, sans-serif",
-        },
-      },
-    },
-    MuiTab: {
-      styleOverrides: {
-        root: {
-          fontFamily: "Anton, sans-serif",
-        },
-      },
-    },
+    fontFamily: "Montserrat, sans-serif",
+    h1: { fontFamily: "Anton, sans-serif" },
+    h2: { fontFamily: "Anton, sans-serif" },
+    h3: { fontFamily: "Anton, sans-serif" },
+    h4: { fontFamily: "Anton, sans-serif" },
+    h5: { fontFamily: "Anton, sans-serif" },
+    h6: { fontFamily: "Anton, sans-serif" },
+    subtitle1: { fontFamily: "Anton, sans-serif" },
+    subtitle2: { fontFamily: "Anton, sans-serif" },
   },
 });
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline /> {/* Normalizes styles across browsers */}
-      <App /> 
+      <App />
     </ThemeProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
